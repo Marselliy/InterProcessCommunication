0aa11c63bedf587f3d667f8dd732a7bf12f66788
Marselliy <sariksariksarik@gmail.com>
2016-05-14 14:04:28 +0300

    Tasks completed

--- tcpServer/client.js
+++ tcpServer/client.js
@@ -3,16 +3,19 @@ global.api = api;
 api.net = require('net');
 
 var socket = new api.net.Socket();
-var user;
-
+var id;
 socket.connect({
   port: 2000,
   host: '127.0.0.1',
 }, function() {
-  socket.write('Hello from client');
   socket.on('data', function(data) {
-    user = JSON.parse(data);
     console.log('Data received (by client): ' + data);
-    console.log('Age of ' + user.name + ' is ' + user.age);
+    id = JSON.parse(data)['id'];
+    send_data = JSON.stringify({id : id, data : JSON.parse(data)['data'].map(function(el){return el * 2;})});
+    socket.write(send_data);
+    console.log('Data sent (by client): ' + send_data);
   });
+  socket.on('error', function(data) {
+    console.log('Error: ' + data['code']);
+  })
 });
--- tcpServer/server.js
+++ tcpServer/server.js
@@ -2,12 +2,35 @@ var api = {};
 global.api = api;
 api.net = require('net');
 
-var user = { name: 'Marcus Aurelius', age: 1895 };
+var work = [32, 2, 4, 54, 3, 6, 17, 24];
+var result = [];
+var users = [];
+var user_count = 2;
+var resp_count;
 
 var server = api.net.createServer(function(socket) {
-  socket.write(JSON.stringify(user));
+  users.push(socket);
   console.log('Connected: ' + socket.localAddress);
+  if (users.length == user_count) {
+    for (var i = 0; i < users.length; i++) {
+      data = JSON.stringify({id : i, data : work.slice(i * work.length / user_count, (i + 1) * work.length / user_count)});
+      users[i].write(data);
+      console.log('Data sent (by server): ' + data);
+    }
+    resp_count = user_count;
+  }
   socket.on('data', function(data) {
     console.log('Data received (by server): ' + data);
+    id = JSON.parse(data)['id'];
+    arr = JSON.parse(data)['data'];
+    for (var i = id * work.length / user_count; i < id * work.length / user_count + arr.length; i++) {
+      result[i] = arr[i - id * work.length / user_count];
+    }
+    resp_count--;
+    if (resp_count == 0)
+      console.log('Result: ' + result);
   });
+  socket.on('error', function(data) {
+    console.log('Error: ' + data['code']);
+  })
 }).listen(2000);
